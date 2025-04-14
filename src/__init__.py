e467bcaf5048116bbdd7050a38af6995cffb1c40
Siva kiran <sivakiran7171@gmail.com>
2025-04-14 10:29:25 +0530

    foldere structure

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+'''
+The purpose of __init__ in Python is to initialize the attributes of an object when it's created from a class. It's essentially the constructor of the class and is automatically called when a new instance of the class is instantiated
+
+'''
