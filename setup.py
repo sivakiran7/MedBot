ac636b78304bd8f34ead3e86bafdc41eec5b9c31
Siva kiran <sivakiran7171@gmail.com>
2025-04-14 10:29:25 +0530

    foldere structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup  # type: ignore
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author='Siva kiran',
+    author_email='sivakiran7171@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+)
+
